d0a5a7fe3ef7f4d66259925cf4338fa268b1d9d0
DarthVader-StarWars <kadzicuh@outlook.com>
2024-12-03 16:03:48 +0300

    Update decompressor.hpp

--- decompressor.hpp
+++ decompressor.hpp
@@ -1,23 +1,36 @@
-#pragma once
-
 #include <windows.h>
 #include <compressapi.h>
 
 #pragma comment(lib, "cabinet.lib")
 
+class Decompressor {
+private:
+    DECOMPRESSOR_HANDLE hDecompressor;
+public:
+    Decompressor(DWORD Algorithm, PCOMPRESS_ALLOCATION_ROUTINES AllocationRoutines) {
+        CreateDecompressor(Algorithm, AllocationRoutines, &this->hDecompressor);
+    }
+    ~Decompressor() {
+        CloseDecompressor(this->hDecompressor);
+    }
+    DECOMPRESSOR_HANDLE getDecompressor() {
+        return this->hDecompressor;
+    }
+};
+
 namespace COMPRESS {
     namespace ALGORITHM {
         namespace XPRESS {
-            size_t decompress(const void* CompressedData, const size_t CompressedDataSize, void** UncompressedBuffer);
+            SIZE_T decompress(CONST BYTE* CompressedData, const SIZE_T CompressedDataSize, BYTE** UncompressedBuffer);
         }
         namespace XPRESS_HUFF {
-            size_t decompress(const void* CompressedData, const size_t CompressedDataSize, void** UncompressedBuffer);
+            SIZE_T decompress(CONST BYTE* CompressedData, const SIZE_T CompressedDataSize, BYTE** UncompressedBuffer);
         }
         namespace MSZIP {
-            size_t decompress(const void* CompressedData, const size_t CompressedDataSize, void** UncompressedBuffer);
+            SIZE_T decompress(CONST BYTE* CompressedData, const SIZE_T CompressedDataSize, BYTE** UncompressedBuffer);
         }
         namespace LZMS {
-            size_t decompress(const void* CompressedData, const size_t CompressedDataSize, void** UncompressedBuffer);
+            SIZE_T decompress(CONST BYTE* CompressedData, const SIZE_T CompressedDataSize, BYTE** UncompressedBuffer);
         }
     }
 }
