6b350cb03048674d37dd01ea42a109b240717ce9
DarthVader-StarWars <kadzicuh@outlook.com>
2024-12-03 16:02:27 +0300

    Update compressor.cpp

--- compressor.cpp
+++ compressor.cpp
@@ -1,32 +1,26 @@
-#include "compressor.hpp"
+#include "Compressor.hpp"
 
-static size_t _compress(const void* UncompressedData, const size_t UncompressedDataSize, void** CompressedBuffer, DWORD Algorithm, PCOMPRESS_ALLOCATION_ROUTINES AllocationRoutines) {
-    COMPRESSOR_HANDLE compressor;
-    CreateCompressor(Algorithm, AllocationRoutines, &compressor);
+static SIZE_T _compress(CONST BYTE* UncompressedData, CONST SIZE_T UncompressedDataSize, BYTE** CompressedBuffer, DWORD Algorithm, PCOMPRESS_ALLOCATION_ROUTINES AllocationRoutines) {
+    Compressor compressor(Algorithm, AllocationRoutines);
     SIZE_T CompressedDataSize;
-    Compress(compressor, UncompressedData, UncompressedDataSize, NULL, 0, &CompressedDataSize);
-    *CompressedBuffer = new void* [CompressedDataSize];
-    Compress(compressor, UncompressedData, UncompressedDataSize, *CompressedBuffer, CompressedDataSize, &CompressedDataSize);
-    CloseCompressor(compressor);
+    Compress(compressor.getCompressor(), UncompressedData, UncompressedDataSize, NULL, 0, &CompressedDataSize);
+    *CompressedBuffer = new BYTE[CompressedDataSize]();
+    Compress(compressor.getCompressor(), UncompressedData, UncompressedDataSize, *CompressedBuffer, CompressedDataSize, &CompressedDataSize);
     return CompressedDataSize;
 }
 
-size_t COMPRESS::ALGORITHM::XPRESS::compress(const void* UncompressedData, const size_t UncompressedDataSize, void** CompressedBuffer)
-{
+SIZE_T COMPRESS::ALGORITHM::XPRESS::compress(CONST BYTE* UncompressedData, CONST SIZE_T UncompressedDataSize, BYTE** CompressedBuffer) {
     return _compress(UncompressedData, UncompressedDataSize, CompressedBuffer, COMPRESS_ALGORITHM_XPRESS, NULL);
 }
 
-size_t COMPRESS::ALGORITHM::XPRESS_HUFF::compress(const void* UncompressedData, const size_t UncompressedDataSize, void** CompressedBuffer)
-{
+SIZE_T COMPRESS::ALGORITHM::XPRESS_HUFF::compress(CONST BYTE* UncompressedData, CONST SIZE_T UncompressedDataSize, BYTE** CompressedBuffer) {
     return _compress(UncompressedData, UncompressedDataSize, CompressedBuffer, COMPRESS_ALGORITHM_XPRESS_HUFF, NULL);
 }
 
-size_t COMPRESS::ALGORITHM::MSZIP::compress(const void* UncompressedData, const size_t UncompressedDataSize, void** CompressedBuffer)
-{
+SIZE_T COMPRESS::ALGORITHM::MSZIP::compress(CONST BYTE* UncompressedData, CONST SIZE_T UncompressedDataSize, BYTE** CompressedBuffer) {
     return _compress(UncompressedData, UncompressedDataSize, CompressedBuffer, COMPRESS_ALGORITHM_MSZIP, NULL);
 }
 
-size_t COMPRESS::ALGORITHM::LZMS::compress(const void* UncompressedData, const size_t UncompressedDataSize, void** CompressedBuffer)
-{
+SIZE_T COMPRESS::ALGORITHM::LZMS::compress(CONST BYTE* UncompressedData, CONST SIZE_T UncompressedDataSize, BYTE** CompressedBuffer) {
     return _compress(UncompressedData, UncompressedDataSize, CompressedBuffer, COMPRESS_ALGORITHM_LZMS, NULL);
 }
