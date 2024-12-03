a6fb6352ee761f350d6c3e1216882eafa88fbcc2
DarthVader-StarWars <kadzicuh@outlook.com>
2024-12-03 17:21:22 +0300

    Update decompressor.cpp

--- decompressor.cpp
+++ decompressor.cpp
@@ -9,19 +9,19 @@ static SIZE_T _decompress(CONST BYTE* CompressedData, CONST SIZE_T CompressedDat
     return UncompressedDataSize;
 }
 
-SIZE_T COMPRESS::ALGORITHM::XPRESS::decompress(CONST BYTE* CompressedData, const SIZE_T CompressedDataSize, BYTE** UncompressedBuffer) {
+SIZE_T COMPRESS::ALGORITHM::XPRESS::decompress(CONST BYTE* CompressedData, CONST SIZE_T CompressedDataSize, BYTE** UncompressedBuffer) {
     return _decompress(CompressedData, CompressedDataSize, UncompressedBuffer, COMPRESS_ALGORITHM_XPRESS, NULL);
 }
 
-SIZE_T COMPRESS::ALGORITHM::XPRESS_HUFF::decompress(CONST BYTE* CompressedData, const SIZE_T CompressedDataSize, BYTE** UncompressedBuffer) {
+SIZE_T COMPRESS::ALGORITHM::XPRESS_HUFF::decompress(CONST BYTE* CompressedData, CONST SIZE_T CompressedDataSize, BYTE** UncompressedBuffer) {
     return _decompress(CompressedData, CompressedDataSize, UncompressedBuffer, COMPRESS_ALGORITHM_XPRESS_HUFF, NULL);
 }
 
-SIZE_T COMPRESS::ALGORITHM::MSZIP::decompress(CONST BYTE* CompressedData, const SIZE_T CompressedDataSize, BYTE** UncompressedBuffer) {
+SIZE_T COMPRESS::ALGORITHM::MSZIP::decompress(CONST BYTE* CompressedData, CONST SIZE_T CompressedDataSize, BYTE** UncompressedBuffer) {
     return _decompress(CompressedData, CompressedDataSize, UncompressedBuffer, COMPRESS_ALGORITHM_MSZIP, NULL);
 }
 
-SIZE_T COMPRESS::ALGORITHM::LZMS::decompress(CONST BYTE* CompressedData, const SIZE_T CompressedDataSize, BYTE** UncompressedBuffer) {
+SIZE_T COMPRESS::ALGORITHM::LZMS::decompress(CONST BYTE* CompressedData, CONST SIZE_T CompressedDataSize, BYTE** UncompressedBuffer) {
     return _decompress(CompressedData, CompressedDataSize, UncompressedBuffer, COMPRESS_ALGORITHM_LZMS, NULL);
 }
 
